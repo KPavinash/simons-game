8d738dfe941c0830fe6c3a5dfed231bb225420bf
KPavinash <avinashpgdi2000>
2023-01-16 18:01:10 +0530

    Fourth commit

--- game.js
+++ game.js
@@ -1,26 +1,67 @@
-var buttonColours=[ "red", "blue", "green", "yellow"];
-var gamePattern= [] ;
-var userClickedPattern= [];
 
+var buttonColours = ["red", "blue", "green", "yellow"];
 
+var gamePattern = [];
+var userClickedPattern = [];
 
-var randomChosenColour = buttonColours[nextSequence()];
-// alert(randomChosenColour);
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
 
-var audioElement = new Audio("sounds/" + randomChosenColour+ ".mp3");
-audioElement.play();
+//2. Create a new variable called level and start at level 0.
+var level = 0;
 
+//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+$(document).keypress(function() {
+  if (!started) {
 
-function nextSequence(){
-    var randomNumber = Math.floor( Math.random()*4);
-    return randomNumber;
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+// user cick
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  setTimeout(nextSequence( checkAnswer(userChosenColour) , 1000));
+    userClickedPattern=[];
+});
+
+function nextSequence() {
+
+  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  level++;
+
+  //5. Inside nextSequence(), update the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
-gamePattern= (randomChosenColour);
 
-// Animation
-$("." + randomChosenColour).delay(5).fadeOut().fadeIn('fast') ;
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
 
 
-
-
-
+function checkAnswer(currentLevel){
+  
+}
